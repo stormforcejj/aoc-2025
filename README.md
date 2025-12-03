e4a93ccbf821f2bed01e2cab9446d64929a1cec3
James <contact@stormforcejj.co.uk>
2025-12-03 15:42:59 +0000

    Day 3 - Part 2

--- README.md
+++ README.md
@@ -13,7 +13,7 @@
 
 [![Day](https://badgen.net/badge/01/%E2%98%85%E2%98%85/green)](src/day01)
 [![Day](https://badgen.net/badge/02/%E2%98%85%E2%98%85/green)](src/day02)
-[![Day](https://badgen.net/badge/03/%E2%98%85%E2%98%86/yellow)](src/day03)
+[![Day](https://badgen.net/badge/03/%E2%98%85%E2%98%85/green)](src/day03)
 ![Day](https://badgen.net/badge/04/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/05/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/06/%E2%98%86%E2%98%86/gray)
@@ -83,9 +83,9 @@ Both parts: 1442.981ms
 
 ```
 Day 03
-Time part 1: 1.18ms
-Time part 2: -
-Both parts: 1.18ms
+Time part 1: 1.203ms
+Time part 2: 7.749ms
+Both parts: 8.952ms
 ```
 
 ```
@@ -243,8 +243,8 @@ Both parts: -
 ```
 
 ```
-Total stars: 5/50
-Total time: 1445.967ms
+Total stars: 6/50
+Total time: 1453.739ms
 ```
 
 <!--/RESULTS-->
--- src/day03/index.ts
+++ src/day03/index.ts
@@ -32,7 +32,33 @@ const part1 = (rawInput: string) => {
 const part2 = (rawInput: string) => {
     const input = parseInput(rawInput);
 
-    return;
+    const banks = input.split("\n");
+    let total = 0;
+
+    for(let i = 0; i<banks.length; i++) {
+        const bank = banks[i].split('');
+        let max : number[] = new Array(12).fill(0);
+        
+        for(let j = 0; j<bank.length; j++) {
+            for(let k = 0; k<max.length; k++) {
+              if(parseInt(bank[j]) > max[k] && j<=(bank.length - (max.length - k))) {
+                max[k] = parseInt(bank[j]);
+                max.fill(0, k+1);
+                break;
+              }
+            }
+        }
+
+        let str = "";
+
+        for(let j = 0; j<max.length; j++) {
+            str = str + max[j].toString();
+        }
+
+        total = total + parseInt(str);
+    }
+
+    return total.toString();
 }
 
 run({
@@ -63,10 +89,26 @@ run({
     },
     part2: {
         tests: [
-        // {
-        //   input: ``,
-        //   expected: "",
-        // },
+        {
+          input: `987654321111111`,
+          expected: "987654321111",
+        },
+        {
+          input: `811111111111119`,
+          expected: "811111111119",
+        },
+        {
+          input: `234234234234278`,
+          expected: "434234234278",
+        },
+        {
+          input: `818181911112111`,
+          expected: "888911112111",
+        },
+        {
+          input: `987654321111111\n811111111111119\n234234234234278\n818181911112111`,
+          expected: "3121910778619",
+        },
         ],
         solution: part2,
     },
