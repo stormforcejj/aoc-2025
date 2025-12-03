f86bdd471ed4d950ca33c88041c5a377b8e20932
James <contact@stormforcejj.co.uk>
2025-12-03 00:40:00 +0000

    Day 2 - Part 2

--- README.md
+++ README.md
@@ -12,7 +12,7 @@
 <!--SOLUTIONS-->
 
 [![Day](https://badgen.net/badge/01/%E2%98%85%E2%98%85/green)](src/day01)
-[![Day](https://badgen.net/badge/02/%E2%98%85%E2%98%86/yellow)](src/day02)
+[![Day](https://badgen.net/badge/02/%E2%98%85%E2%98%85/green)](src/day02)
 ![Day](https://badgen.net/badge/03/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/04/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/05/%E2%98%86%E2%98%86/gray)
@@ -76,9 +76,9 @@ Both parts: 1.805ms
 
 ```
 Day 02
-Time part 1: 202.757ms
-Time part 2: -
-Both parts: 202.757ms
+Time part 1: 203.803ms
+Time part 2: 1239.179ms
+Both parts: 1442.981ms
 ```
 
 ```
@@ -243,8 +243,8 @@ Both parts: -
 ```
 
 ```
-Total stars: 3/50
-Total time: 204.562ms
+Total stars: 4/50
+Total time: 1444.787ms
 ```
 
 <!--/RESULTS-->
--- src/day02/index.ts
+++ src/day02/index.ts
@@ -11,10 +11,10 @@ const part1 = (rawInput: string) => {
     for(let i = 0; i<ranges.length; i++) {
         const bounds = ranges[i].split('-');
 
-        for(let i = parseInt(bounds[0]); i <= parseInt(bounds[1]); i++) {
-            const str = i.toString();
+        for(let j = parseInt(bounds[0]); j <= parseInt(bounds[1]); j++) {
+            const str = j.toString();
             if(str.substring(0, str.length/2) == str.substring(str.length/2, str.length)) {
-                total = total + i;
+                total = total + j;
             }
         }
     }
@@ -25,7 +25,27 @@ const part1 = (rawInput: string) => {
 const part2 = (rawInput: string) => {
     const input = parseInput(rawInput);
 
-    return;
+    const ranges = input.split(",");
+    let total = 0;
+
+    for (let i = 0; i < ranges.length; i++) {
+        const bounds = ranges[i].split("-");
+
+        for (let j = parseInt(bounds[0]); j <= parseInt(bounds[1]); j++) {
+            const str = j.toString();
+
+            for(let k = 1; k <= str.length / 2; k++) {
+                const substr = str.substring(0,k);
+
+                if(str.split(substr).filter(e => e !== '').length == 0) {
+                    total = total  + j;
+                    break;
+                }
+            }
+        }
+    }
+
+    return total.toString();
 }
 
 run({
@@ -72,10 +92,54 @@ run({
     },
     part2: {
         tests: [
-            // {
-            //   input: ``,
-            //   expected: "",
-            // },
+            {
+                input: `11-22`,
+                expected: "33",
+            },
+            {
+                input: `95-115`,
+                expected: "210",
+            },
+            {
+                input: `998-1012`,
+                expected: "2009",
+            },
+            {
+                input: `1188511880-1188511890`,
+                expected: "1188511885",
+            },
+            {
+                input: `222220-222224`,
+                expected: "222222",
+            },
+            {
+                input: `1698522-1698528`,
+                expected: "0",
+            },
+            {
+                input: `446443-446449`,
+                expected: "446446",
+            },
+            {
+                input: `38593856-38593862`,
+                expected: "38593859",
+            },
+            {
+                input: `565653-565659`,
+                expected: "565656",
+            },
+            {
+                input: `824824821-824824827`,
+                expected: "824824824",
+            },
+            {
+                input: `2121212118-2121212124`,
+                expected: "2121212121",
+            },
+            {
+                input: `11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124`,
+                expected: "4174379265",
+            },
         ],
         solution: part2,
     },
