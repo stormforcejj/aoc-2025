737fe4163fb5074f14af2ab8ccac4c6a2989c044
James <contact@stormforcejj.co.uk>
2025-12-08 18:10:21 +0000

    Day 7 - Part 2

--- README.md
+++ README.md
@@ -17,7 +17,7 @@
 [![Day](https://badgen.net/badge/04/%E2%98%85%E2%98%85/green)](src/day04)
 [![Day](https://badgen.net/badge/05/%E2%98%85%E2%98%85/green)](src/day05)
 [![Day](https://badgen.net/badge/06/%E2%98%85%E2%98%85/green)](src/day06)
-[![Day](https://badgen.net/badge/07/%E2%98%85%E2%98%86/yellow)](src/day07)
+[![Day](https://badgen.net/badge/07/%E2%98%85%E2%98%85/green)](src/day07)
 ![Day](https://badgen.net/badge/08/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/09/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/10/%E2%98%86%E2%98%86/gray)
@@ -111,9 +111,9 @@ Both parts: 3.384ms
 
 ```
 Day 07
-Time part 1: 0.703ms
-Time part 2: -
-Both parts: 0.703ms
+Time part 1: 2.23ms
+Time part 2: 2.984ms
+Both parts: 5.214ms
 ```
 
 ```
@@ -243,8 +243,8 @@ Both parts: -
 ```
 
 ```
-Total stars: 13/50
-Total time: 1470.421ms
+Total stars: 14/50
+Total time: 1474.932ms
 ```
 
 <!--/RESULTS-->
--- src/day07/index.ts
+++ src/day07/index.ts
@@ -33,9 +33,68 @@ const part1 = (rawInput: string) => {
 }
 
 const part2 = (rawInput: string) => {
-    const input = parseInput(rawInput);
+    const input = parseInput(rawInput).split("\n");
+    const pathsFrom = new Map<string, number>()
 
-    return;
+    let vals = [];
+
+    for (let i = 0; i < input.length; i++) {
+        vals[i] = input[i].split('');
+    }
+
+    let count = 0;
+    let descendFrom = 0;
+
+    for(let i = 0; i < vals[0].length; i++) {
+        if(vals[0][i] == "S") {
+            descendFrom = i;
+        }
+    }
+
+    for(let i = 0; i < vals.length; i++) {
+        if(vals[i][descendFrom] == "^") {
+            count = count + split(descendFrom, i, vals, pathsFrom, true);
+            count = count + split(descendFrom, i, vals, pathsFrom, false)
+            break;
+        }
+
+        if(vals[i][descendFrom] == "." && i == vals.length-1) {
+            count = 1;
+            break;
+        }
+    }
+
+    return count.toString();
+}
+
+function split(x: number, y: number, vals: string[][], pathsFrom: Map<string, number>, left : boolean) {
+    let count = 0;
+    if(left) {
+        x = x - 1;
+    } else {
+        x = x + 1;
+    }
+    
+    y++;
+    while (y < vals.length) {
+        if (y == vals.length - 1 && vals[y][x] == ".") {
+            return 1;
+        }
+        
+        if(vals[y][x] == "^") {
+            if (pathsFrom.has([x,y].toString())) {
+                count = count + (pathsFrom.get([x,y].toString()) || 0);
+                return count;
+            }
+            count = count + split(x, y, vals, pathsFrom, true);
+            count = count + split(x, y, vals, pathsFrom, false);
+            pathsFrom.set([x,y].toString(), count);
+            return count;
+        }
+        
+        y++;
+    }
+    return 1;
 }
 
 run({
@@ -50,10 +109,22 @@ run({
     },
     part2: {
         tests: [
-            // {
-            //   input: ``,
-            //   expected: "",
-            // },
+            {
+                input: `.......S.......\n...............\n.......^.......\n...............\n......^.^......\n...............\n.....^.^.^.....\n...............\n....^.^...^....\n...............\n...^.^...^.^...\n...............\n..^...^.....^..\n...............\n.^.^.^.^.^...^.\n...............`,
+                expected: "40",
+            },
+            {
+                input: `..S..\n.....\n..^..\n.....`,
+                expected: "2"
+            },
+            {
+                input: `..S..\n.....\n..^..\n.....\n.^.^.`,
+                expected: "4"
+            },
+            {
+                input: `...S...\n.......\n...^...\n.......\n..^.^..\n.......\n.^.^.^.`,
+                expected: "8"
+            }
         ],
         solution: part2,
     },
